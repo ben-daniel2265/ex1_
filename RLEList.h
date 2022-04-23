47a9b4b527593706af0b57603779481f13981878
ben-daniel2265 <102544798+ben-daniel2265@users.noreply.github.com>
2022-04-23 23:59:20 +0300

    added get and map

--- RLEList.h
+++ RLEList.h
@@ -179,28 +179,37 @@ RLEListResult RLEListAppend(RLEList list, char value){
     if(!list || !value){
         return RLE_LIST_NULL_ARGUMENT;
     }
+
     while(list->next){
         list = list->next;
     }
+
     RLEList node = (RLEList)malloc(sizeof(*node));
     if(!node){
         return RLE_LIST_OUT_OF_MEMORY;
     }
+
     node->data = value;
     list->next = node;
-    if(list->next->data == value){
-        return RLE_LIST_SUCCESS;
-    }
+    list->next->data == value;
+
+    return RLE_LIST_SUCCESS;
 }
+
 int RLEListSize(RLEList list){
+    printf("l");
     int count = 0;
     if(!list){
         return -1;
     }
-    while(list){
+
+    printf("l");
+    while(list->next != NULL){
         list = list->next;
         count++;
     }
+    printf("l");
+    return count;
 }
 
 /**
@@ -228,3 +237,119 @@ RLEListResult RLEListRemove(RLEList list, int index){
     list->next = list->next->next;
     return RLE_LIST_SUCCESS;
 }
+
+/**
+*   RLEListGet: Returns the character found at a specified index in an RLE list.
+*
+* @param list - The RLE list to retrieve the character from.
+* @param index - The index at which the character to be retrieved is found.
+* @param result - Pointer to be used to store the result of the operation, if it is NULL, the result will not be saved.
+* 	RLE_LIST_NULL_ARGUMENT if a NULL was sent to the function as list.
+* 	RLE_LIST_INDEX_OUT_OF_BOUNDS if given index is not withing the list's bounds.
+* 	LIST_SUCCESS the character found at index has been retrieved successfully.
+* @return
+* 	0 if result is not RLE_LIST_SUCCESS.
+* 	The character found at given index in case of success.   
+*/
+char RLEListGet(RLEList list, int index, RLEListResult *result){
+    printf("l");
+    if(!list){
+        *result = RLE_LIST_NULL_ARGUMENT;
+        return 0;
+    }
+    
+    if(!(0 <= index && index < RLEListSize(list))){
+        *result = RLE_LIST_INDEX_OUT_OF_BOUNDS;
+        return 0;
+    }
+
+    
+    while(index >= 0){
+        list = list->next;
+        index -= 1;
+    }
+
+    *result = RLE_LIST_SUCCESS;
+    return list->data;
+}
+
+/**
+*   RLEListExportToString: Returns the characters found in an RLE list as a string.
+*
+* @param list - The RLE list to be exported as a string.
+* @param result - Pointer to be used to store the result of the operation, if it is NULL, the result will not be saved.
+* 	RLE_LIST_NULL_ARGUMENT if a NULL was sent to the function as list.
+* 	LIST_SUCCESS the RLE list has been successfuly exported as a string.
+* @return
+* 	NULL if result is not RLE_LIST_SUCCESS.
+* 	The string that corresponds to the received RLE list.
+
+
+int first_digit(int num){
+    while(num > 10){
+        num = num / 10;
+    }
+    return num;
+}
+
+char* RLEListExportToString(RLEList list, RLEListResult* result){
+    if(!list){
+        *result = RLE_LIST_NULL_ARGUMENT;
+        return NULL;
+    }
+
+    int count_chunks = 1;
+
+    RLEList temp1 = list;
+    RLEList temp2 = list->next;
+
+    int chunk_size = 1;
+    int index = 0;
+    int numsize = 1;
+    temp1 = list;
+    temp2 = list->next;
+
+    while(temp2){
+        if(!(temp1->data == temp2->data)){
+            result[index] = temp1->data;
+            while(chunk_size != 0){
+                result[index + numsize] = first_digit(chunk_size);
+                chunk_size = chunk_size / 10;
+                numsize++;
+            }
+            result[index + numsize] = '\n';
+            chunk_size = 1;
+            numsize = 1;
+        }
+
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+    }
+
+    return result;
+}*/
+
+/**
+*   RLEListMap: Change the given RLE list's characters according to the received mapping function.
+*               This function replaces each character of the give RLE list with its mapped character.
+*
+* @param list - The RLE list to edit.
+* @param MapFunction - Pointer to a function of type MapFunction. 
+* @return
+* 	RLE_LIST_NULL_ARGUMENT if a NULL was sent as a paramater.
+* 	LIST_SUCCESS if the mapping is done successfully.
+*/
+RLEListResult RLEListMap(RLEList list, MapFunction map_function){
+    if(!list){
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+
+    while(list){
+        list->data = map_function(list->data);
+        list = list->next;
+    }
+
+    return RLE_LIST_SUCCESS;
+}
+
+
