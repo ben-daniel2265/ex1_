a9a9cc6d955e05a450c4cc00d08debf958e5720c
yonatanelbaz <73475019+yonatanelbaz@users.noreply.github.com>
2022-04-24 21:27:32 +0300

    Update RLEList.c

--- RLEList.c
+++ RLEList.c
@@ -73,17 +73,6 @@ int RLEListSize(RLEList list){
     return count;
 }
 
-/**
-*   RLEListRemove: Removes a character found at a specified index in an RLE list.
-*
-* @param list - The RLE list to remove the character from.
-* @param index - The index at which the character to be removed is found.
-* @return
-* 	RLE_LIST_NULL_ARGUMENT if a NULL was sent to the function.
-* 	RLE_LIST_INDEX_OUT_OF_BOUNDS if given index is not withing the list's bounds.
-* 	RLE_LIST_SUCCESS the character found at index has been removed successfully.
-*/
-
 RLEListResult RLEListRemove(RLEList list, int index){
     if(!list || !index){
         return RLE_LIST_NULL_ARGUMENT;
@@ -99,19 +88,7 @@ RLEListResult RLEListRemove(RLEList list, int index){
     return RLE_LIST_SUCCESS;
 }
 
-/**
-*   RLEListGet: Returns the character found at a specified index in an RLE list.
-*
-* @param list - The RLE list to retrieve the character from.
-* @param index - The index at which the character to be retrieved is found.
-* @param result - Pointer to be used to store the result of the operation, if it is NULL, the result will not be saved.
-* 	RLE_LIST_NULL_ARGUMENT if a NULL was sent to the function as list.
-* 	RLE_LIST_INDEX_OUT_OF_BOUNDS if given index is not withing the list's bounds.
-* 	LIST_SUCCESS the character found at index has been retrieved successfully.
-* @return
-* 	0 if result is not RLE_LIST_SUCCESS.
-* 	The character found at given index in case of success.
-*/
+
 char RLEListGet(RLEList list, int index, RLEListResult *result){
     if(!list){
         *result = RLE_LIST_NULL_ARGUMENT;
@@ -133,17 +110,6 @@ char RLEListGet(RLEList list, int index, RLEListResult *result){
     return list->data;
 }
 
-/**
-*   RLEListExportToString: Returns the characters found in an RLE list as a string.
-*
-* @param list - The RLE list to be exported as a string.
-* @param result - Pointer to be used to store the result of the operation, if it is NULL, the result will not be saved.
-* 	RLE_LIST_NULL_ARGUMENT if a NULL was sent to the function as list.
-* 	LIST_SUCCESS the RLE list has been successfuly exported as a string.
-* @return
-* 	NULL if result is not RLE_LIST_SUCCESS.
-* 	The string that corresponds to the received RLE list.
-*/
 
 int first_digit(int num){
     while(num >= 10){
@@ -196,16 +162,7 @@ char* RLEListExportToString(RLEList list, RLEListResult* result) {
     }
     return string;
 }
-/**
-*   RLEListMap: Change the given RLE list's characters according to the received mapping function.
-*               This function replaces each character of the give RLE list with its mapped character.
-*
-* @param list - The RLE list to edit.
-* @param MapFunction - Pointer to a function of type MapFunction.
-* @return
-* 	RLE_LIST_NULL_ARGUMENT if a NULL was sent as a paramater.
-* 	LIST_SUCCESS if the mapping is done successfully.
-*/
+
 RLEListResult RLEListMap(RLEList list, MapFunction map_function){
     if(!list){
         return RLE_LIST_NULL_ARGUMENT;
