3fd706eb1bce6b16ef14c4084be59172deb983b9
ben-daniel2265 <102544798+ben-daniel2265@users.noreply.github.com>
2022-04-24 22:13:54 +0300

    Update RLEList.h
    reset

--- RLEList.h
+++ RLEList.h
@@ -7,7 +7,6 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
 
 /**
 * Run Length Encoding List
@@ -29,12 +28,7 @@
 /** 
 * Typedef for defining the RLE list.
 * Complete the implementation of struct RLEList_t defined in RLEList.c
- *
-*/typedef struct RLEList_t{
-    char data;
-    struct RLEList_t* next;
-} *RLEList;
-
+*/
 typedef struct RLEList_t *RLEList;
 
 /** Enum used for returning error codes from RLE list functions */
@@ -45,6 +39,7 @@ typedef enum {
     RLE_LIST_INDEX_OUT_OF_BOUNDS,
     RLE_LIST_ERROR
 } RLEListResult;
+
 
 /** 
  * Type of function for mapping characters.
@@ -154,234 +149,3 @@ RLEListResult RLEListMap(RLEList list, MapFunction map_function);
 
 
 #endif // HW1_RLELIST_H
-
-RLEList RLEListCreate(){
-    RLEList list = (RLEList)malloc(sizeof(*list));
-    if(!list){
-        return NULL;
-    }
-    list->data = NULL;
-    list->next = NULL;
-
-    return list;
-}
-
-void RLEListDestroy(RLEList list){
-    if(!list){
-        return;
-    }
-    while(list) {
-        RLEList toDelete = list;
-        list = list->next;
-        free(toDelete);
-    }
-}
-
-RLEListResult RLEListAppend(RLEList list, char value){
-    if(!list || !value){
-        return RLE_LIST_NULL_ARGUMENT;
-    }
-
-    while(list->next){
-        list = list->next;
-    }
-
-    RLEList node = (RLEList)malloc(sizeof(*node));
-    if(!node){
-        return RLE_LIST_OUT_OF_MEMORY;
-    }
-
-    node->data = value;
-    list->next = node;
-    list->next->data == value;
-
-    return RLE_LIST_SUCCESS;
-}
-
-int RLEListSize(RLEList list){
-    int count = 1;
-    if(!list){
-        return -1;
-    }
-
-    while(list->next != NULL){
-        list = list->next;
-        count++;
-    }
-    return count;
-}
-
-/**
-*   RLEListRemove: Removes a character found at a specified index in an RLE list.
-*
-* @param list - The RLE list to remove the character from.
-* @param index - The index at which the character to be removed is found.
-* @return
-* 	RLE_LIST_NULL_ARGUMENT if a NULL was sent to the function.
-* 	RLE_LIST_INDEX_OUT_OF_BOUNDS if given index is not withing the list's bounds.
-* 	RLE_LIST_SUCCESS the character found at index has been removed successfully.
-*/
-
-RLEListResult RLEListRemove(RLEList list, int index){
-    if(!list || !index){
-        return RLE_LIST_NULL_ARGUMENT;
-    }
-    int bounds = RLEListSize(list);
-    if(index > bounds){
-        return RLE_LIST_INDEX_OUT_OF_BOUNDS;
-    }
-    for(int i = 0; i < index; i++){
-        list = list->next;
-    }
-    list->next = list->next->next;
-    return RLE_LIST_SUCCESS;
-}
-
-/**
-*   RLEListGet: Returns the character found at a specified index in an RLE list.
-*
-* @param list - The RLE list to retrieve the character from.
-* @param index - The index at which the character to be retrieved is found.
-* @param result - Pointer to be used to store the result of the operation, if it is NULL, the result will not be saved.
-* 	RLE_LIST_NULL_ARGUMENT if a NULL was sent to the function as list.
-* 	RLE_LIST_INDEX_OUT_OF_BOUNDS if given index is not withing the list's bounds.
-* 	LIST_SUCCESS the character found at index has been retrieved successfully.
-* @return
-* 	0 if result is not RLE_LIST_SUCCESS.
-* 	The character found at given index in case of success.   
-*/
-char RLEListGet(RLEList list, int index, RLEListResult *result){
-    if(!list){
-        *result = RLE_LIST_NULL_ARGUMENT;
-        return 0;
-    }
-    
-    if(!(0 <= index && index < RLEListSize(list))){
-        *result = RLE_LIST_INDEX_OUT_OF_BOUNDS;
-        return 0;
-    }
-
-    
-    while(index > 0){
-        list = list->next;
-        index -= 1;
-    }
-
-    *result = RLE_LIST_SUCCESS;
-    return list->data;
-}
-
-/**
-*   RLEListExportToString: Returns the characters found in an RLE list as a string.
-*
-* @param list - The RLE list to be exported as a string.
-* @param result - Pointer to be used to store the result of the operation, if it is NULL, the result will not be saved.
-* 	RLE_LIST_NULL_ARGUMENT if a NULL was sent to the function as list.
-* 	LIST_SUCCESS the RLE list has been successfuly exported as a string.
-* @return
-* 	NULL if result is not RLE_LIST_SUCCESS.
-* 	The string that corresponds to the received RLE list.
-*/
-
-int first_digit(int num){
-    while(num >= 10){
-        num = num / 10;
-    }
-    return num;
-}
-
-char* RLEListExportToString(RLEList list, RLEListResult* result){
-    if(!list){
-        *result = RLE_LIST_NULL_ARGUMENT;
-        return NULL;
-    }
-
-    int count_chunks = 1;
-
-    char* string = (char *)malloc(sizeof(char)*RLEListSize(list)*4);
-    RLEList temp1 = list;
-    RLEList temp2 = list->next;
-
-    int str_len = 0;
-    int chunk_size = 1;
-    int index = 0;
-    temp1 = list;
-    temp2 = list->next;
-
-    while(temp2){
-        if(!(temp1->data == temp2->data)){
-            string[str_len] = temp1->data;
-            str_len++;
-            while(chunk_size != 0){
-                string[str_len] = 48 + first_digit(chunk_size);
-                str_len++;
-                chunk_size = chunk_size / 10;
-            }
-            string[str_len] = '\n';
-            str_len++;
-            chunk_size = 0;
-        }
-
-        chunk_size++;
-
-
-        if(!(temp2->next)){
-            if(!(temp1->data == temp2->data)){
-                string[str_len] = temp2->data;
-                str_len++;
-                string[str_len] = 48 + 1;
-                str_len++;
-                string[str_len] = '\n';
-                str_len++;
-            }
-        }
-
-        temp1 = temp1->next;
-        temp2 = temp2->next;
-    }
-
-    if(str_len == 0){
-        string[str_len] = list->data;
-        str_len++;
-        
-        while(chunk_size != 0){
-            string[str_len] = 48 + first_digit(chunk_size);
-            str_len++;
-            chunk_size = chunk_size / 10;
-        }
-        string[str_len] = '\n';
-    }
-
-    string[str_len] = '\0';
-
-
-    string = (char*)realloc(string, str_len);
-
-    *result = RLE_LIST_SUCCESS;
-    return string;
-}
-
-/**
-*   RLEListMap: Change the given RLE list's characters according to the received mapping function.
-*               This function replaces each character of the give RLE list with its mapped character.
-*
-* @param list - The RLE list to edit.
-* @param MapFunction - Pointer to a function of type MapFunction. 
-* @return
-* 	RLE_LIST_NULL_ARGUMENT if a NULL was sent as a paramater.
-* 	LIST_SUCCESS if the mapping is done successfully.
-*/
-RLEListResult RLEListMap(RLEList list, MapFunction map_function){
-    if(!list){
-        return RLE_LIST_NULL_ARGUMENT;
-    }
-
-    while(list){
-        list->data = map_function(list->data);
-        list = list->next;
-    }
-
-    return RLE_LIST_SUCCESS;
-}
-
-
